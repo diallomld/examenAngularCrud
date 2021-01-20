efb807917205847992526328f0e4fe286f47c47d
diallomld <diallomld5@gmail.com>
2021-01-20 23:28:48 +0000

    last commit

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: 'AIzaSyDrETs5s_gb8HjHwkE8YYiGSZrP_sIyONM',
+    authDomain: 'crud-angular-unipro.firebaseapp.com',
+    databaseURL: 'https://crud-angular-unipro-default-rtdb.firebaseio.com',
+    projectId: 'crud-angular-unipro',
+    storageBucket: 'crud-angular-unipro.appspot.com',
+    messagingSenderId: '177667207706',
+    appId: '1:177667207706:web:1458366f18b16fe24457a3'
+  }
 };
