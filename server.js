809b7a4224f944cadcb45fac458bd33ae4f10ae3
diallomld <diallomld5@gmail.com>
2021-01-21 01:14:39 +0000

    last commit

--- server.js
+++ server.js
@@ -5,10 +5,10 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static('./dist/examen-angular'));
+app.use(express.static('./dist/courses'));
 
 app.get('/*', (req, res) =>
-    res.sendFile('index.html', {root: 'dist/examen-angular/'}),
+    res.sendFile('index.html', {root: 'dist/courses/'}),
 );
 
 // Start the app by listening on the default Heroku port
